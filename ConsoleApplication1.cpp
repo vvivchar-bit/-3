49c351105ad2de2920bb0af203679ae89775a088
vvivchar-bit <v.vivchar@istu.edu.ua>
2026-03-22 12:21:36 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Figure
+{
+public:
+    virtual double Area() const = 0;
+    virtual void Show() const = 0;
+    virtual ~Figure() {}
+};
+
+class Triangle : public Figure
+{
+private:
+    double base;
+    double height;
+
+public:
+    Triangle(double b, double h) : base(b), height(h) {}
+
+    double Area() const override
+    {
+        return 0.5 * base * height;
+    }
+
+    void Show() const override
+    {
+        cout << "Triangle: base = " << base
+            << ", height = " << height
+            << ", area = " << Area() << endl;
+    }
+};
+
+class Rectangle : public Figure
+{
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle(double w, double h) : width(w), height(h) {}
+
+    double Area() const override
+    {
+        return width * height;
+    }
+
+    void Show() const override
+    {
+        cout << "Rectangle: width = " << width
+            << ", height = " << height
+            << ", area = " << Area() << endl;
+    }
+};
+
+class Circle : public Figure
+{
+private:
+    double radius;
+
+public:
+    Circle(double r) : radius(r) {}
+
+    double Area() const override
+    {
+        return 3.14159 * radius * radius;
+    }
+
+    void Show() const override
+    {
+        cout << "Circle: radius = " << radius
+            << ", area = " << Area() << endl;
+    }
+};
+
+int main()
+{
+    vector<Figure*> figures;
+
+    figures.push_back(new Triangle(6, 4));
+    figures.push_back(new Rectangle(5, 3));
+    figures.push_back(new Circle(2.5));
+
+    cout << "Polymorphism demonstration:" << endl;
+    for (Figure* f : figures)
+    {
+        f->Show();
+    }
+
+    for (Figure* f : figures)
+    {
+        delete f;
+    }
+
+    return 0;
+}
---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+using namespace std;
+
+class Convert
+{
+protected:
+    double value1;
+    double value2;
+
+public:
+    Convert(double v)
+    {
+        value1 = v;
+    }
+
+    double GetInitial() const
+    {
+        return value1;
+    }
+
+    double GetConverted() const
+    {
+        return value2;
+    }
+
+    virtual void Compute() = 0;
+    virtual ~Convert() {}
+};
+
+class LitersToGallons : public Convert
+{
+public:
+    LitersToGallons(double v) : Convert(v) {}
+
+    void Compute() override
+    {
+        value2 = value1 / 3.7854;
+    }
+};
+
+class FahrenheitToCelsius : public Convert
+{
+public:
+    FahrenheitToCelsius(double v) : Convert(v) {}
+
+    void Compute() override
+    {
+        value2 = (value1 - 32) / 1.8;
+    }
+};
+
+int main()
+{
+    Convert* p;
+
+    LitersToGallons litersObj(4);
+    FahrenheitToCelsius tempObj(70);
+
+    p = &litersObj;
+    cout << p->GetInitial() << " liters = ";
+    p->Compute();
+    cout << p->GetConverted() << " gallons" << endl;
+
+    p = &tempObj;
+    cout << p->GetInitial() << " Fahrenheit = ";
+    p->Compute();
+    cout << p->GetConverted() << " Celsius" << endl;
+
+    return 0;
+}
